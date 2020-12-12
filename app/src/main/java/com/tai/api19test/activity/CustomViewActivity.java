d71debb41acebb0844352771a2096c9b0816e95b
MingZ266 <mingzheng266@outlook.com>
2020-12-13 02:18:01 +0800

    保存头像裁剪框得到的图片

--- app/src/main/java/com/tai/api19test/activity/CustomViewActivity.java
+++ app/src/main/java/com/tai/api19test/activity/CustomViewActivity.java
@@ -1,12 +1,17 @@
 package com.tai.api19test.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,17 +21,31 @@ import com.tai.api19test.util.Tools;
 import com.tai.api19test.view.CropPictureView;
 import com.tai.api19test.view.MyImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CustomViewActivity extends AppCompatActivity {
     private final AppCompatActivity activity = this;
     private static final String TAG = "CustomViewTAG";
     private final int choosePicRequestCode = 266;
+    private boolean turnDo = true;
+    private Bitmap cutBitmap;
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss", Locale.getDefault());
 
     private CropPictureView cropPicture;
     private Button choosePic;
     private Button sure;
     private MyImageView showCropPic;
+    private Button save;
+    private Button look;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +61,8 @@ public class CustomViewActivity extends AppCompatActivity {
         choosePic = findViewById(R.id.choosePic);
         sure = findViewById(R.id.sure);
         showCropPic = findViewById(R.id.showCropPic);
+        save = findViewById(R.id.save);
+        look = findViewById(R.id.look);
     }
 
     private void myListener() {
@@ -57,7 +78,92 @@ public class CustomViewActivity extends AppCompatActivity {
         sure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showCropPic.setImageBitmap(cropPicture.getCropPicBitmap());
+                cutBitmap = cropPicture.getCropPicBitmap();
+                showCropPic.setImageBitmap(cutBitmap);
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cutBitmap != null) {
+                    Log.d(TAG, "Bitmap图片尺寸：" + cutBitmap.getByteCount() + "或" + cutBitmap.getAllocationByteCount());
+                    String filename = dateFormat.format(System.currentTimeMillis());
+                    File cutPicPNGFile = Tools.generateFileAtFiles(activity, null, filename + ".png", true);
+                    File cutPicCodePNGFile = Tools.generateFileAtFiles(activity, null, filename + ".png.txt", true);
+                    File cutPicJPEGFile = Tools.generateFileAtFiles(activity, null, filename + ".jpeg", true);
+                    File cutPicCodeJPEGFile = Tools.generateFileAtFiles(activity, null, filename + ".jpeg.txt", true);
+                    try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+                        if (cutBitmap.compress(Bitmap.CompressFormat.PNG, 100, bos)) {
+                            byte[] picBytes = bos.toByteArray();
+                            Tools.saveFile(cutPicCodePNGFile, Base64.encodeToString(picBytes, Base64.NO_WRAP));
+                            try (FileOutputStream fos = new FileOutputStream(cutPicPNGFile)) {
+                                fos.write(picBytes);
+                                Toast.makeText(activity, "已存储PNG", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(activity, "存储PNG失败", Toast.LENGTH_SHORT).show();
+                        }
+                        bos.reset();
+                        if (cutBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos)) {
+                            byte[] picBytes = bos.toByteArray();
+                            Tools.saveFile(cutPicCodeJPEGFile, Base64.encodeToString(picBytes, Base64.NO_WRAP));
+                            try (FileOutputStream fos = new FileOutputStream(cutPicJPEGFile)) {
+                                fos.write(picBytes);
+                                Toast.makeText(activity, "已存储JPEG", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(activity, "存储JPEG失败", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (IOException e) {
+                        Log.d(TAG, "存储文件失败");
+                    }
+                }
+            }
+        });
+
+        look.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final File file = getExternalFilesDir("");
+                if (file != null) {
+                    final String[] filenames = file.list();
+                    if (filenames != null) {
+                        new Thread() {
+                            @Override
+                            public void run() {
+                                Bitmap bitmap;
+                                for (String filename : filenames) {
+                                    if (filename.endsWith(".jpeg.txt")) {
+                                        String code = Tools.readFile(new File(file.getAbsolutePath(), filename));
+                                        if (code != null) {
+                                            byte[] bytes = Base64.decode(code, Base64.NO_WRAP);
+                                            bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                            setPic(bitmap);
+                                            try {
+                                                sleep(2000);
+                                            } catch (InterruptedException e) {
+                                                // ignore
+                                            }
+                                        }
+                                    }
+                                }
+                                setPic(null);
+                            }
+
+                            private void setPic(final Bitmap bitmap) {
+                                showCropPic.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        showCropPic.setImageBitmap(bitmap);
+                                    }
+                                });
+                            }
+                        }.start();
+                        return;
+                    }
+                }
+                Toast.makeText(activity, "查看图片失败", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -69,14 +175,27 @@ public class CustomViewActivity extends AppCompatActivity {
             if (data != null) {
                 Uri uri = data.getData();
                 if (uri != null) {
-                    /*try {
-                        cropPicture.setSrcPic(uri);
-                    } catch (FileNotFoundException e) {
-                        Log.d(TAG, "Uri转图片失败");
-                    }*/
-                    cropPicture.setSrcPic(Tools.uri2Bitmap(activity, uri));
+                    if (turnDo) {
+                        turnDo = false;
+                        try {
+                            cropPicture.setSrcPic(uri);
+                        } catch (FileNotFoundException e) {
+                            Log.d(TAG, "Uri转图片失败");
+                        }
+                    } else {
+                        turnDo = true;
+                        cropPicture.setSrcPic(Tools.uri2Bitmap(activity, uri));
+                    }
                 }
             }
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (cutBitmap != null) {
+            cutBitmap.recycle();
+        }
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/tai/api19test/util/Tools.java
+++ app/src/main/java/com/tai/api19test/util/Tools.java
@@ -6,20 +6,32 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 
 public class Tools {
-    public static String selfStartCfg = "SelfStart.cfg";
+    public static final String selfStartCfg = "SelfStart.cfg";
 
-    private static String TAG = "ToolsTAG";
+    private static final String TAG = "ToolsTAG";
+    private static final Gson gson = new Gson();
 
     /**
      * 测量View的宽高.
@@ -104,23 +116,36 @@ public class Tools {
         }
     }
 
+    private static <E> List<E> getListFromJson(String json, Class<E> eClass) {
+        try {
+            List<E> list = new LinkedList<>();
+            JSONArray jsonArray = new JSONArray(json);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                list.add(gson.fromJson(String.valueOf(jsonArray.getJSONObject(i)), eClass));
+            }
+            return list;
+        } catch (JSONException e) {
+            Log.d(TAG, "解析为List结构失败，原因是：" + e.getClass().getSimpleName() + ":" + e.getMessage());
+            return null;
+        }
+    }
+
+    //========================文件==========================
     /**
-     * 进行简单配置的存储.
-     * 存储路径为 data/包名/shared_prefs/ 下
+     * 简单配置的存储.
+     * 存储路径为 /data/user/0/包名/shared_prefs/ 下
      *
      * @param context 需要存储配置的Activity上下文
-     * @param fileName 文件名
-     * @param keyAndValue 以key，value，key，value，...这种方式的存储值
-     *                    若为奇数个，将舍掉最后一个
+     * @param fileName 配置的文件名
+     * @param keyAndValue 以key，value，key，value，...这种方式的存储值序列。
+     *                    若为值数目奇数个，将舍掉最后一个
      */
     public static void saveSharedPreferences(Context context, String fileName, String... keyAndValue) {
         if (keyAndValue.length % 2 == 1) {
-            String[] newStrings = new String[keyAndValue.length - 1];
-            System.arraycopy(keyAndValue, 0, newStrings, 0, newStrings.length);
-            keyAndValue = newStrings;
+            keyAndValue = Arrays.copyOf(keyAndValue, keyAndValue.length - 1);
         }
-        SharedPreferences sp = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
+        SharedPreferences read = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = read.edit();
         for (int i = 0; i < keyAndValue.length; i += 2) {
             editor.putString(keyAndValue[i], keyAndValue[i + 1]);
         }
@@ -135,6 +160,209 @@ public class Tools {
         SharedPreferences.Editor editor = sp.edit();
         editor.clear();
         editor.apply();
+    }
+
+    /**
+     * 对较大的数据进行文件存储.
+     *
+     * @param file 用来存储数据的文件，原文件内容将被覆盖。
+     *             若文件不存在，将会创建。
+     * @param data 将被存储的数据
+     * @return 若文件存储成功，则返回存储数据的文件引用；否则返回null。
+     */
+    public static File saveFile(File file, String data) {
+        if (file == null || data == null) {
+            Log.d(TAG, "保存文件失败：参数含空");
+            return null;
+        }
+        File parent = file.getParentFile();
+        if (parent != null && !parent.exists()) {
+            if (!parent.mkdirs())
+                return null;
+        }
+        try {
+            if (file.exists() || file.createNewFile()) {
+                try (FileOutputStream fos = new FileOutputStream(file)) {
+                    fos.write(data.getBytes());
+                    return file;
+                }
+            } else {
+                return null;
+            }
+        } catch (IOException e) {
+            Log.d(TAG, "保存文件发生异常：" + e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * 从文件中读取数据.
+     *
+     * @param file 指定的要读取数据的文件。
+     * @return 若读取成功，则返回读取的结果；否则返回null。
+     */
+    public static String readFile(File file) {
+        if (file == null) {
+            Log.d(TAG, "读取文件失败：参数为空");
+            return null;
+        }
+        try (FileInputStream fis = new FileInputStream(file)) {
+            int len;
+            byte[] cache = new byte[1024];
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((len = fis.read(cache)) != -1) {
+                stringBuilder.append(new String(cache, 0, len));
+            }
+            return String.valueOf(stringBuilder);
+        } catch (IOException e) {
+            Log.d(TAG, "读取文件发生异常：" + e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * 将对象保存到文件.
+     *
+     * @param file 用来存储数据的文件，原文件内容将被覆盖。
+     *             若文件不存在，将会创建。
+     * @param object 保存的对象
+     * @param <T> 任意对象类型
+     * @return 若文件存储成功，则返回存储数据的文件引用；否则返回null。
+     */
+    public static <T> File saveObjectAtFile(File file, T object) {
+        return saveFile(file, gson.toJson(object));
+    }
+
+    /**
+     * 从文件读取对象.
+     *
+     * @param file 指定的要读取对象的文件
+     * @param tClass 读取的对象类型
+     * @param <T> 任意对象类型
+     * @return 若读取成功，则返回读取的对象；否则返回null。
+     */
+    public static <T> T readObjectFromFile(File file, Class<T> tClass) {
+        String readData = readFile(file);
+        if (readData != null)
+            return gson.fromJson(readData, tClass);
+        else
+            return null;
+    }
+
+    /**
+     * 从文件中读取List对象.
+     *
+     * @param file 指定的要读取对象的文件
+     * @param eClass 读取的List集合元素类型
+     * @param <E> 任意对象类型
+     * @return 若读取成功，则返回读取的List集合；否则返回null
+     */
+    public static <E> List<E> readListFromFile(File file, Class<E> eClass) {
+        String data = readFile(file);
+        if (data == null) {
+            return null;
+        }
+        return getListFromJson(data, eClass);
+    }
+
+    /**
+     * 通过指定的各个属性生成cache目录下的文件引用.
+     *
+     * @param directory 存储的文件目录
+     * @param filename 存储的文件名
+     * @param atExternal true表示保存在外部存储设备；false表示保存在内部存储
+     * @return 返回生成的文件引用
+     */
+    public static File generateFileAtCache(Context context, String directory, String filename, boolean atExternal) {
+        String path;
+        if (atExternal)
+            path = getExternalCachePath(context);
+        else
+            path = getInternalCachePath(context);
+        if (!TextUtils.isEmpty(directory)) {
+            if (directory.lastIndexOf('/') == directory.length() - 1)// 若末尾有'/'，则去除
+                directory = directory.substring(0, directory.length() - 1);
+            if (directory.indexOf('/') == 0)
+                path += directory;
+            else
+                path += ("/" + directory);
+        }
+        return new File(path, filename);
+    }
+
+    /**
+     * 通过指定的各个属性生成files目录下的文件引用.
+     *
+     * @param directory 存储的文件目录
+     * @param filename 存储的文件名
+     * @param atExternal true表示保存在外部存储设备；false表示保存在内部存储
+     * @return 返回生成的文件引用
+     */
+    public static File generateFileAtFiles(Context context, String directory, String filename, boolean atExternal) {
+        String path;
+        if (atExternal)
+            path = getExternalFilesPath(context);
+        else
+            path = getInternalFilesPath(context);
+        if (!TextUtils.isEmpty(directory)) {
+            if (directory.lastIndexOf('/') == directory.length() - 1)// 若末尾有'/'，则去除
+                directory = directory.substring(0, directory.length() - 1);
+            if (directory.indexOf('/') == 0)
+                path += directory;
+            else
+                path += ("/" + directory);
+        }
+        return new File(path, filename);
+    }
+
+    //// 内部存储(internal)，路径一般为：
+    ////         /data/user/0/包名/...
+    /**
+     * 获得内部存储中的cache目录路径.
+     *
+     * @return 返回内部存储中的cache目录路径
+     */
+    public static String getInternalCachePath(Context context) {
+        return context.getCacheDir().getAbsolutePath();
+    }
+
+    /**
+     * 获得内部存储中的files目录路径.
+     *
+     * @return 返回内部存储中的files目录路径.
+     */
+    public static String getInternalFilesPath(Context context) {
+        return context.getFilesDir().getAbsolutePath();
+    }
+
+    //// 外部存储(external)，路径一般为：
+    ////         /storage/emulated/0/Android/data/包名/...
+    /**
+     * 获得外部存储中的cache目录路径.
+     *
+     * @return 返回外部存储中的cache目录路径；
+     *         若获取为空，则返回内部存储中的cache目录路径
+     */
+    public static String getExternalCachePath(Context context) {
+        File cache = context.getExternalCacheDir();
+        if (cache != null)
+            return context.getExternalCacheDir().getAbsolutePath();
+        else
+            return getInternalCachePath(context);
+    }
+
+    /**
+     * 获得外部存储中的files目录路径.
+     *
+     * @return 返回外部存储中的files目录路径；
+     *         若获取为空，则返回外部存储中的files目录路径
+     */
+    public static String getExternalFilesPath(Context context) {
+        File file = context.getExternalFilesDir("");
+        if (file != null)
+            return context.getExternalFilesDir("").getAbsolutePath();
+        else
+            return getInternalFilesPath(context);
     }
 
     //---------------------图片、Uri、Bitmap----------------------
